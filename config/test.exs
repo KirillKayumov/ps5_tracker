8caee9cc68f059b3b4b116a809a0c13b75f2ed3f
Kirill Kaiumov <kirill.kayumov@uula.io>
2020-12-03 16:53:58 +0100

    Fix test env

--- config/test.exs
+++ config/test.exs
@@ -5,12 +5,34 @@ use Mix.Config
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
-config :ps5, Ps5.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "ps5_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+# config :ps5, Ps5.Repo,
+#   username: "postgres",
+#   password: "postgres",
+#   database: "ps5_test#{System.get_env("MIX_TEST_PARTITION")}",
+#   hostname: "localhost",
+#   pool: Ecto.Adapters.SQL.Sandbox
+
+if System.get_env("DATABASE_URL") do
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
+
+  config :ps5, Ps5.Repo,
+    ssl: true,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    pool: Ecto.Adapters.SQL.Sandbox
+else
+  config :ps5, Ps5.Repo,
+    username: "postgres",
+    password: "postgres",
+    database: "ps5_test#{System.get_env("MIX_TEST_PARTITION")}",
+    hostname: "localhost",
+    pool: Ecto.Adapters.SQL.Sandbox
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
